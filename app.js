0d709788cfa02361e9a3bbeb1c5e4db634df269f
HsiehWei <william1992507@gmail.com>
2022-12-20 01:08:49 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose') //載入 mongoose
 const restaurantList = require('./restaurant.json').results
+
+//僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+// 連線成功
+db.once('open', () =>{
+  console.log('mongodb connected')
+})
+
 const app = express()
 const port = 3000
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs'}))
+app.set('view engine', 'hbs')
 app.use(express.static('public'))
+
+mongoose.connect(process.env.MONGOOSE_URI, { useNewUrlParser: true, useUnifiedTopology: true }) //連線到mongoDB
 
 //render reataurant
 app.get('/', (req, res) => {
