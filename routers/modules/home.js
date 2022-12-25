013e97b7a0a9a630571c949cbb6c24dad785111c
HsiehWei <william1992507@gmail.com>
2022-12-25 18:02:51 +0800

    feat: add sort

--- routers/modules/home.js
+++ routers/modules/home.js
@@ -7,6 +7,7 @@ const Restaurant = require('../../models/restaurant')
 router.get('/', (req, res) => {
   Restaurant.find()
     .lean()
+    .sort({_id: 'asc'})
     .then(restaurants => res.render('index', { restaurants }))
     .catch(err => console.error(error))
 })
@@ -15,17 +16,34 @@ router.get('/', (req, res) => {
 router.get('/search', (req, res) => {
 
   const keywords = req.query.keywords
+  const sort = req.query.sort
   const reataurantType = req.query.keywords.toLowerCase()
 
 
+  const sortData = {
+    0: {_id: 'asc'},
+    atoz: {name: 'asc'},
+    ztoa: {name: 'desc'},
+    category: { category: 'asc'},
+    location: {location: 'asc'}
+  }
+
+  const sortSelected = { [sort]: true }
+  // console.log(sortData[sort])
   Restaurant.find({})
     .lean()
+    .sort(sortData[sort])
     .then(restaurants => {
       const restaurantFilter = restaurants.filter(restaurant =>
         restaurant.name.toLowerCase().includes(reataurantType) ||
         restaurant.category.includes(reataurantType)
       )
-      res.render('index', { restaurants: restaurantFilter, keyword: keywords })
+      if(keywords) {
+        res.render('index', { restaurants: restaurantFilter, keyword: keywords, sortSelected })
+      } else {
+        res.render('index', { restaurants: restaurantFilter, sortSelected })
+      }
+
     })
     .catch(err => console.log(error))
 })
